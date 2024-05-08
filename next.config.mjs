1895f74602c5dadc2db3fe5d0232d3bf60a31921
HafizhIslami <hafizhislami11@gmail.com>
2024-05-08 14:50:25 +0700

    changes in DB_LOCAL_URI

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    DB_LOCAL_URI: "mongodb://localhost:27017/next-project",
+    DB_LOCAL_URI: "mongodb://localhost:27017/nextjs-project",
     DB_URI: "",
   },
 };
