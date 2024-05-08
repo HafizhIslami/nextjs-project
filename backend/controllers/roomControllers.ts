8ba03482147fe4e7f64b600c039c84d0dc450811
HafizhIslami <hafizhislami11@gmail.com>
2024-05-08 14:49:52 +0700

    room crud features done

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -22,3 +22,56 @@ export const newRoom = async (req: NextRequest) => {
     room,
   });
 };
+
+export const getRoomDetail = async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const room = await Room.findById(params.id);
+
+  if (!room) {
+    return NextResponse.json({ message: "Room not found" }, { status: 404 });
+  }
+  return NextResponse.json({
+    success: true,
+    room,
+  });
+};
+
+export const updateRoom = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  let room = await Room.findById(params.id);
+  const body = await req.json();
+
+  if (!room) {
+    return NextResponse.json({ message: "Room not found" }, { status: 404 });
+  }
+
+  room = await Room.findByIdAndUpdate(params.id, body, {
+    new: true,
+  });
+
+  return NextResponse.json({
+    success: true,
+    room,
+  });
+};
+
+export const deleteRoom = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const room = await Room.findById(params.id);
+
+  if (!room) {
+    return NextResponse.json({ message: "Room not found" }, { status: 404 });
+  }
+
+  await room.deleteOne();
+
+  return NextResponse.json({
+    success: true,
+  });
+};
