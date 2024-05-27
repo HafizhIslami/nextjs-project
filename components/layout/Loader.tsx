6dc31714a76564f246832d6830a7bb194ee0c68c
HafizhIslami <hafizhislami11@gmail.com>
2024-05-27 13:57:19 +0700

    add loading component

--- components/layout/Loader.tsx
+++ components/layout/Loader.tsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="loader">
-      <div className="loader__spinner"></div>
+    <div className="d-flex justify-content-center center-loader">
+      <div className="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
