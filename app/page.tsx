c1da97a9163e2a4fddff57829fdfc29296a86ca9
HafizhIslami <hafizhislami11@gmail.com>
2024-06-10 11:54:02 +0700

    update app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
-  return (
-    <div className="container">
-      <h1>HOME</h1>
-    </div>
-  );
+import Home from "@/components/Home";
+import Error from "./error";
+
+export const dynamic = "force-dynamic";
+
+export const metadata = {
+  title: "HomePage - BookIT",
+};
+
+const getRooms = async (searchParams: string) => {
+  const urlParams = new URLSearchParams(searchParams);
+  const queryString = urlParams.toString();
+  const res = await fetch(`${process.env.API_URL}/api/rooms?${queryString}`, {cache: "no-cache"});
+  return res.json();
+};
+
+export default async function HomePage({
+  searchParams,
+}: {
+  searchParams: string;
+}) {
+  const data = await getRooms(searchParams);
+
+  if (data?.errMessage) {
+    <Error error={data} />;
+  }
+  return <Home data={data} />;
 }
