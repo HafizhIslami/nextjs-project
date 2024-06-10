775b446f13c116e64434b90749c711ac909ea7bf
HafizhIslami <hafizhislami11@gmail.com>
2024-06-10 11:15:29 +0700

    changes on GlobalProvider

--- app/GlobalProvider.tsx
+++ app/GlobalProvider.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+"use client";
+
+import { store } from "@/redux/store";
+import { SessionProvider } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 
 export function GlobalProvider({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Toaster />
+      <Provider store={store}>
+        <SessionProvider>{children}</SessionProvider>
+      </Provider>
+    </>
+  );
 }
