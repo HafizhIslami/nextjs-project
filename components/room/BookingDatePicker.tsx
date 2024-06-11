a76682c97fb87586c6d09bddb05a6782b76a8692
HafizhIslami <hafizhislami11@gmail.com>
2024-06-11 09:52:31 +0700

    update BookingDatePicker for room component

--- components/room/BookingDatePicker.tsx
+++ components/room/BookingDatePicker.tsx
@@ -1,17 +1,41 @@
+import { IRoom } from "@/backend/models/room";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+
 interface Props {
-  room: any;
+  room: IRoom;
 }
-const BookingDatePicker = ({room}:Props) => {
-  
+const BookingDatePicker = ({ room }: Props) => {
+  const [checkInDate, setCheckInDate] = useState(new Date());
+  const [checkOutDate, setCheckOutDate] = useState(new Date());
+
+  const onChangeHandler = (dates: Date[]) => {
+    const [checkInDate, checkOutDate] = dates;
+
+    setCheckInDate(checkInDate);
+    setCheckOutDate(checkOutDate);
+    
+    console.log(checkInDate, checkOutDate);
+  };
 
   return (
     <div className="booking-card shadow p-4">
       <p className="price-per-night">
-        <b>$100</b> / night
+        <b>$ {room?.pricePerNight}</b> / night
       </p>
       <hr />
       <p className="mt-5 mb-3">Pick Check In & Check Out Date</p>
+      <DatePicker
+        className="w-100"
+        selected={checkInDate}
+        onChange={onChangeHandler}
+        startDate={checkInDate}
+        endDate={checkOutDate}
+        minDate={new Date()}
+        selectsRange
+        inline
+      />
     </div>
   );
 };
- export default BookingDatePicker;
+export default BookingDatePicker;
