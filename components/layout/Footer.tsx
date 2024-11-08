3d478c46197beca5838567a27491b408eade0da9
HafizhIslami <hafizhislami11@gmail.com>
2024-11-08 11:17:55 +0700

    add show password icon in update password

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="py-1 pt-5">
       <p className="text-center mt-1">
-        ProjectName - year, All Rights Reserved
+        Roomi - 2024
       </p>
     </footer>
   );
