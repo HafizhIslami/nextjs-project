8c39fbdc91db73822adc1cf12158c8bd8b9c80bf
HafizhIslami <hafizhislami11@gmail.com>
2026-09-14 00:30:54 +0700

    update: dbConnect.ts remove comment

--- backend/config/dbConnect.ts
+++ backend/config/dbConnect.ts
@@ -1,38 +1,3 @@
-// import mongoose from "mongoose";
-
-// type DbConnectOptions = {
-//   throwOnError?: boolean;
-// };
-
-// declare global {
-//   // eslint-disable-next-line no-var
-//   var mongoosePromise: Promise<typeof mongoose> | undefined;
-// }
-
-// const dbConnect = async (options: DbConnectOptions = {}) => {
-//   if (mongoose.connection.readyState >= 1) {
-//     return;
-//   }
-
-//   const DB_URI = process.env.DB_URI || process.env.DB_LOCAL_URI;
-//   console.log("DB_URI:", DB_URI);
-//   if (!DB_URI) {
-//     const error = new Error("Database connection string (DATABASE_URI/DB_URI) is missing in .env");
-//     if (options.throwOnError) {
-//       throw error;
-//     }
-//     return;
-//   }
-
-//   if (!global.mongoosePromise) {
-//     global.mongoosePromise = mongoose.connect(DB_URI);
-//   }
-
-//   await global.mongoosePromise;
-//   console.log("DB Connected successfully to:", DB_URI.split("@")[1] || "Localhost");
-// };
-
-// export default dbConnect;
 import mongoose from "mongoose";
 
 type DbConnectOptions = {
