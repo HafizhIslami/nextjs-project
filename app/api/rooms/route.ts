72650cefad255d8cc53ce8528a41ef20264c5bed
HafizhIslami <hafizhislami11@gmail.com>
2026-09-14 01:49:29 +0700

    update: show vercel IP

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -2,6 +2,14 @@ import { allRooms, newRoom } from "@/backend/controllers/roomControllers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
+  try {
+    const ipRes = await fetch('https://api.ipify.org?format=json', { cache: 'no-store' });
+    const ipData = await ipRes.json();
+    console.log('=== IP VERCEL KAMU Saat Ini ===:', ipData.ip);
+  } catch (e) {
+    console.error('Gagal fetch IP', e);
+  }
+
     return await allRooms(request, { params: { entries: "6" } });
 }
 
